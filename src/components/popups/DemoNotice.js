0dba636efd3544983bcc0a45e36c0f56169b8a17
maciejkuran <maciejkuran@gmail.com>
2022-12-10 12:51:31 +0100

    created portal to DemoNotice modal

--- src/components/popups/DemoNotice.js
+++ src/components/popups/DemoNotice.js
@@ -3,21 +3,33 @@ import Card from '../UI/Card';
 import CloseButton from '../UI/CloseButton';
 import BackgroundOverlay from '../UI/BackgroundOverlay';
 import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
 const DemoNotice = props => {
   const hideDemoNoticePopupHandler = () => {
     props.setDemoNoticePopup(false);
   };
 
-  return (
+  return ReactDOM.createPortal(
     <Fragment>
       <Card className={classes['demo-notice']}>
         <h3>Sorry, it's a demo version! This feature is not available. 👀</h3>
         <CloseButton onClick={hideDemoNoticePopupHandler} />
       </Card>
       <BackgroundOverlay onClick={hideDemoNoticePopupHandler} />
-    </Fragment>
+    </Fragment>,
+    document.getElementById('demo-modal')
   );
 };
 
 export default DemoNotice;
+
+{
+  /* <Fragment>
+      <Card className={classes['demo-notice']}>
+        <h3>Sorry, it's a demo version! This feature is not available. 👀</h3>
+        <CloseButton onClick={hideDemoNoticePopupHandler} />
+      </Card>
+      <BackgroundOverlay onClick={hideDemoNoticePopupHandler} />
+    </Fragment> */
+}
